4f11f2966dc09f29738b4282aa59e1b91b795cc9
me-miheer <me.miheer@gmail.com>
2024-01-25 14:08:21 +0530

    added

---
+++ GlobalFunctions/routePermission.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const UserDetails = require("../GlobalFunctions/userDetails")
+
+const routePermissions = async (userId,allowedRolesList) => {
+    try {
+        if(!allowedRolesList){
+            return false
+        }
+    
+        let user = await UserDetails(userId)
+    
+        if(!user){
+            return false
+        }
+        
+        const userPermission = user.rolePermissions[0].name
+
+        if(allowedRolesList.includes(userPermission)){
+            return true
+        }
+
+        return false
+    } catch (error) {
+        return false
+    }
+
+}
+
+module.exports = routePermissions
