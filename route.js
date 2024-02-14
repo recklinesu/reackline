5956dadb9da302b69179c6129ec16429cdc33d02
me-miheer <me.miheer@gmail.com>
2024-02-14 16:31:29 +0530

    added

--- route.js
+++ route.js
@@ -4,6 +4,11 @@ const routesInit = (app)=>{
     app.get("/", (req, res)=>{
       res.send("Welcome to the Reckline API");
     })
+
+    // Serve Socket.IO client library
+    app.get('/socket.io/socket.io.js', (req, res) => {
+      res.sendFile(__dirname + 'node_modules/socket.io/client-dist/socket.io.js');
+    });
   
     app.use("/api", require("./routes/domain"));
     app.use("/api/auth", require("./routes/auth"));
--- socket/users.js
+++ socket/users.js
@@ -1,23 +1,44 @@
 // socketInit.js
-
 const socketIO = require('socket.io');
+const Users = require("../models/user");
+const mongoose = require('mongoose');
 
 function socketInit(server) {
-    const io = socketIO(server);
+    const io = socketIO(server, {
+        cors: true
+    });
 
     // Define event handlers
-    io.on('connection', (socket) => {
-        console.log('A user connected');
+    io.on('connection', async (socket) => {
+        
+        if(mongoose.Types.ObjectId.isValid(socket.handshake.auth.userId)){
+            await Users.findByIdAndUpdate(new mongoose.Types.ObjectId(socket.handshake.auth.userId), {online: true})
+            socket.broadcast.emit("onlineUser", { userId: socket.handshake.auth.userId, online: true})
+        }
 
-        // Handle disconnect event
-        socket.on('disconnect', () => {
-            console.log('User disconnected');
+        
+        socket.on("reconnect", async () => {
+            if(mongoose.Types.ObjectId.isValid(socket.handshake.auth.userId)){
+                await Users.findByIdAndUpdate(new mongoose.Types.ObjectId(socket.handshake.auth.userId), {online: true})
+                socket.broadcast.emit("onlineUser", { userId: socket.handshake.auth.userId, online: false})
+            }
         });
+        
+        socket.on("disconnect", async ()=>{
+            if(mongoose.Types.ObjectId.isValid(socket.handshake.auth.userId)){
+                await Users.findByIdAndUpdate(new mongoose.Types.ObjectId(socket.handshake.auth.userId), {online: false})
+                socket.broadcast.emit("onlineUser", { userId: socket.handshake.auth.userId, online: false})
+            }
+        })
 
-        // Handle custom events
-        socket.on('some_event', (data) => {
-            console.log('Received data:', data);
+
+        socket.on("connect_error", async () => {
+            if(mongoose.Types.ObjectId.isValid(socket.handshake.auth.userId)){
+                await Users.findByIdAndUpdate(new mongoose.Types.ObjectId(socket.handshake.auth.userId), {online: false})
+                socket.broadcast.emit("onlineUser", { userId: socket.handshake.auth.userId, online: false})
+            }
         });
+        
     });
 }
 
