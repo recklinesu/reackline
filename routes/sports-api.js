f5dbe0052d1d9c1cc49746f13bcc6b585054ebe4
me-miheer <me.miheer@gmail.com>
2024-02-29 17:16:09 +0530

    added

--- routes/sports-api.js
+++ routes/sports-api.js
@@ -9,6 +9,7 @@ const routes = express.Router();
 routes.get("/get-data", (req, res) => {
     request.get({
         url: req.body.api,
+        forever: false
     }, function(error, response, body) {
         if (!error && response.statusCode === 200) {
             // console.log(body);
