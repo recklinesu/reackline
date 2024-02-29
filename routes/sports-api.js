1b9384315e7ae428f589c5b71ed4fbc6b88570ff
me-miheer <me.miheer@gmail.com>
2024-02-29 16:34:19 +0530

    added

--- routes/sports-api.js
+++ routes/sports-api.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const request = require('request');
 require("dotenv").config();
 
 const routes = express.Router();
 
-routes.get("/get-data", (req, res)=>{
-    fetch("http://142.93.36.1/api/v1/fetch_data?Action=listEventTypes")
-    .then(x => console.log(x))
-    .then(y => res.json(y));
-    // res.send("hello")
-})
+
+routes.get("/get-data", (req, res) => {
+    request.get({
+        url: 'http://142.93.36.1/api/v1/fetch_data?Action=listEventTypes'
+    }, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log(body);
+            res.send(body);
+        } else {
+            console.error("Error:", error);
+            res.status(response.statusCode).send("Error fetching data");
+        }
+    });
+});
 
 module.exports = routes
